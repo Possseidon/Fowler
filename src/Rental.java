fe745b23b5720f8ede29deb3b0423de4e2d236aa
Dominik Vogel <xpossseidon@gmail.com>
2017-05-09 11:46:04 +0200

    formatting  fixed

---
+++ src/Rental.java
@@ -0,0 +1,21 @@
+class Rental 
+{	
+    private Movie movie;
+    private int daysRented;
+    
+    public Rental(Movie newMovie, int newDaysRented) 
+    {
+        movie = newMovie;
+        daysRented = newDaysRented;
+    }
+    
+    public int getDaysRented() 
+    {
+        return daysRented;
+    }
+    
+    public Movie getMovie() 
+    {
+        return movie;
+    }
+}
